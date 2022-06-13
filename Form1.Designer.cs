7ba0693dfd13e0af7dad073ffa3a63afa4f9dac7
gusus21 <yanenko.yurii1121@vu.cdu.edu.ua>
2022-06-13 13:12:54 +0300

    block3

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace _3block
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(213, 29);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(582, 550);
+            this.panel1.TabIndex = 9;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 382);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 49);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 443);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 49);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 256);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 94);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Confirm Points";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 591);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3block
+{
+    public partial class Form1 : Form
+    {
+        Label[] lb = new Label[6];
+        TextBox[] tb = new TextBox[6];
+        Graphics g;
+        PointF[] pt = new PointF[3];
+        PointF[] ptFi = new PointF[3];
+        static PointF pt0;
+        Pen p1 = new Pen(Color.Black);
+        float fii;
+        float coeff;
+        int numClick;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            coeff = 0;
+            timer1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            coeff = 0;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            tb[2 * numClick].Text = e.X.ToString();
+            tb[2 * numClick + 1].Text = e.Y.ToString();
+            numClick += 1;
+            if (numClick == 3)
+                numClick = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                lb[i] = new Label();
+                lb[i].Location = new Point(10, 45 + 30 * i);
+                lb[i].Size = new Size(25, 25);
+                if (i % 2 == 0)
+                    lb[i].Text = "x" + (i / 2 + 1);
+                else
+                    lb[i].Text = "y" + (i / 2 + 1);
+                lb[i].TextAlign = ContentAlignment.MiddleCenter;
+                Controls.Add(lb[i]);
+
+                tb[i] = new TextBox();
+                tb[i].Location = new Point(40, 45 + 30 * i);
+                tb[i].Size = new Size(35, 25);
+                tb[i].TextAlign = HorizontalAlignment.Center;
+                tb[i].ReadOnly = true;
+                Controls.Add(tb[i]);
+            }
+            numClick = 0;
+            g = panel1.CreateGraphics();
+        }
+
+        static PointF Triangle_Top(float fi, PointF ptt)
+        {
+            float dx = ptt.X - pt0.X;
+            float dy = ptt.Y - pt0.Y;
+            float dxn = dx * (float)Math.Cos(fi) - dy * (float)Math.Sin(fi);
+            float dyn = dx * (float)Math.Sin(fi) + dy * (float)Math.Cos(fi);
+            PointF pt = new PointF(pt0.X + dxn, pt0.Y + dyn);
+            return pt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            for (int i = 0; i < 3; i++)
+                pt[i] = new PointF(float.Parse(tb[2 * i].Text), float.Parse(tb[2 * i + 1].Text));
+            g.DrawPolygon(p1, pt);
+            pt0 = new PointF((pt[0].X + pt[1].X + pt[2].X) / 3.0f, (pt[0].Y + pt[1].Y + pt[2].Y) / 3.0f);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            fii = coeff * (float)Math.PI;
+            for (int i1 = 0; i1 < 3; i1++)
+            {
+                ptFi[i1] = Triangle_Top(fii, pt[i1]);
+            }
+            g.DrawPolygon(p1, ptFi);
+            coeff += 0.05f;
+            if (coeff == 2)
+                coeff = 0;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            tb[2 * numClick].Text = e.X.ToString();
+            tb[2 * numClick + 1].Text = e.Y.ToString();
+            numClick += 1;
+            if (numClick == 3)
+                numClick = 0;
+        }
+    }
+}
